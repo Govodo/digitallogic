72ca512661bff1d6828acfb049de737d90c1ada2
aixoio <79969530+aixoio@users.noreply.github.com>
2023-10-11 19:24:50 +1100

    Replace NewBit with MakeNewBit in logic.go

--- logic.go
+++ logic.go
@@ -2,12 +2,12 @@ package digitallogic
 
 // OR
 
-func (a *Bit) Or(b *Bit) Bit {
-	return NewBit(a.GetBit() || b.GetBit())
+func (a *Bit) Or(b *Bit) *Bit {
+	return MakeNewBit(a.GetBit() || b.GetBit())
 }
 
 // NOR
 
-func (a *Bit) Nor(b *Bit) Bit {
-	return NewBit(!(a.GetBit() || b.GetBit()))
+func (a *Bit) Nor(b *Bit) *Bit {
+	return MakeNewBit(!(a.GetBit() || b.GetBit()))
 }
