759fd89bea4a0e64fa3f15079ae17aec3ced21e9
aixoio <79969530+aixoio@users.noreply.github.com>
2023-10-11 19:34:42 +1100

    Add AND and NAND

--- logic.go
+++ logic.go
@@ -11,3 +11,15 @@ func (a *Bit) Or(b *Bit) *Bit {
 func (a *Bit) Nor(b *Bit) *Bit {
 	return MakeNewBit(!(a.GetBit() || b.GetBit()))
 }
+
+// AND
+
+func (a *Bit) And(b *Bit) *Bit {
+	return MakeNewBit(a.GetBit() && b.GetBit())
+}
+
+// NAND
+
+func (a *Bit) Nand(b *Bit) *Bit {
+	return MakeNewBit(!(a.GetBit() && b.GetBit()))
+}
--- logic_test.go
+++ logic_test.go
@@ -1,6 +1,8 @@
 package digitallogic
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBit_Or(t *testing.T) {
 
@@ -24,6 +26,33 @@ func TestBit_Nor(t *testing.T) {
 
 	if c.GetBit() {
 		t.Error("Nor does not equal 0 when a is 1 and b is 0")
+	}
+
+}
+
+func TestBit_And(t *testing.T) {
+
+	a := NewBit(true)
+	b := NewBit(false)
+
+	c := a.And(&b)
+
+	if c.GetBit() {
+		t.Error("And does not equal 0 when a is 1 and b is 0")
+		
+	}
+
+}
+
+func TestBit_Nand(t *testing.T) {
+
+	a := NewBit(true)
+	b := NewBit(false)
+
+	c := a.Nand(&b)
+	
+	if !c.GetBit() {
+		t.Error("Nand does not equal 1 when a is 1 and b is 0")
 	}
 
 }
